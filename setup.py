87be972d67ab89b0fd2fb6cd452123d9bcd8305b
igor-feoktistov <ifeoktistov@yahoo.com.com>
2014-08-08 15:48:00 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='infoblox',
+    version='1.0',
+    description='The module implements Infoblox IPAM API via REST API',
+    long_description=README,
+    license = 'Licensed under the Apache License, Version 2.0',
+    packages=['.'],
+    install_requires=['requests'],
+)
